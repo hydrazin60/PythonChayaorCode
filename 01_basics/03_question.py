273f681188d293e61247ba76ff4eff89a2abd1f4
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-28 18:51:19 +0545

    03question small update  {function end in python}

--- 01_basics/03_question.py
+++ 01_basics/03_question.py
@@ -1,17 +1,27 @@
 studentMarks = int(input("Enter your marks: "))
 
-if(studentMarks >= 90):
+if(studentMarks > 100):
+   print("Invalid marks")
+
+elif(studentMarks < 0):
+   
+  if(studentMarks >= 90):
    grade = "A"
-elif(90 > studentMarks >= 80):
- grade = "B"
-elif(80 > studentMarks >= 70):
+  elif(90 > studentMarks >= 80):
+    grade = "B"
+  elif(80 > studentMarks >= 70):
     grade = "C"
-elif(70 > studentMarks >= 60):
+  elif(70 > studentMarks >= 60):
     grade = "D"
-elif(60 > studentMarks >= 50):
+  elif(60 > studentMarks >= 50):
     grade = "E"
-else:
-    grade = "F"
+  else:
+    grade = "F"   
+  print("Your grade is ", grade)   
 
 
-print("Your grade is ", grade)
+   
+ 
+
+
+ 
