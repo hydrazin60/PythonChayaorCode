5b0d4c6d91cdd8d0ab18f1988111c22c73009206
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-28 18:45:44 +0545

    03_question solve

---
+++ 01_basics/03_question.py
@@ -0,0 +1,17 @@
+studentMarks = int(input("Enter your marks: "))
+
+if(studentMarks >= 90):
+   grade = "A"
+elif(90 > studentMarks >= 80):
+ grade = "B"
+elif(80 > studentMarks >= 70):
+    grade = "C"
+elif(70 > studentMarks >= 60):
+    grade = "D"
+elif(60 > studentMarks >= 50):
+    grade = "E"
+else:
+    grade = "F"
+
+
+print("Your grade is ", grade)
