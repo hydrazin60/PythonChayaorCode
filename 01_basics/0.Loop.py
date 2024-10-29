935358bffde550a4712c03e266f16ef16aa3760b
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-29 16:34:30 +0545

    0.2 question solve

---
+++ 01_basics/0.Loop.py
@@ -0,0 +1,8 @@
+totalNumber = int(input("Enter a number: "))
+sumOfEvenNumber = 0
+
+for i in range(totalNumber + 1):
+    if(i % 2 ==0):
+        sumOfEvenNumber = sumOfEvenNumber + i
+print(sumOfEvenNumber)
+
--- 01_basics/03_question.py
+++ 01_basics/03_question.py
@@ -2,22 +2,22 @@ studentMarks = int(input("Enter your marks: "))
 
 if(studentMarks > 100):
    print("Invalid marks")
-
-elif(studentMarks < 0):
-   
-  if(studentMarks >= 90):
+   exec(quit())
+ 
+if(studentMarks >= 90):
    grade = "A"
-  elif(90 > studentMarks >= 80):
+elif(90 > studentMarks >= 80):
     grade = "B"
-  elif(80 > studentMarks >= 70):
+elif(80 > studentMarks >= 70):
     grade = "C"
-  elif(70 > studentMarks >= 60):
+elif(70 > studentMarks >= 60):
     grade = "D"
-  elif(60 > studentMarks >= 50):
+elif(60 > studentMarks >= 50):
     grade = "E"
-  else:
-    grade = "F"   
-  print("Your grade is ", grade)   
+else:
+    grade = "F"  
+
+print("Your grade is ", grade)   
 
 
    
