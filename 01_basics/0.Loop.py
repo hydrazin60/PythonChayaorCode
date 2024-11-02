71154043dfa7da873b324a973b65e7a657cc8c3f
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-02 16:55:51 +0545

    0.1 function question solve

--- 01_basics/0.Loop.py
+++ 01_basics/0.Loop.py
@@ -1,8 +1,12 @@
-totalNumber = int(input("Enter a number: "))
-sumOfEvenNumber = 0
+# totalNumber = int(input("Enter a number: "))
+# sumOfEvenNumber = 0
 
-for i in range(totalNumber + 1):
-    if(i % 2 ==0):
-        sumOfEvenNumber = sumOfEvenNumber + i
-print(sumOfEvenNumber)
+# for i in range(totalNumber + 1):
+#     if(i % 2 ==0):
+#         sumOfEvenNumber = sumOfEvenNumber + i
+# print(sumOfEvenNumber)
 
+##3 question
+
+ 
+ 
---
+++ 01_basics/function/functionQuestion.py
@@ -0,0 +1,10 @@
+## question :-1
+n = int(input("Enter a number: "))
+square = 0
+
+def squareofNumber(n):
+    square = n * n
+    return square
+
+result = (n)
+print("Square of the number is : ",  result)
